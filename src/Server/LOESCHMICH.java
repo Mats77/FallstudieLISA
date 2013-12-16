5f46ccab8281d2f5304e16463c845a6b598c0f79
Mats77 <m.wieben@yahoo.de>
2013-12-16 20:08:01 +0100

    Neuer Test

--- src/Server/LOESCHMICH.java
+++ src/Server/LOESCHMICH.java
@@ -2,6 +2,16 @@ package Server;
 
 public class LOESCHMICH {
 	
-	//JETZT STEHT HIER EIN 
+	public static void neueMethode()
+	{
+		System.out.println("Chris stinkt");
+	}	
 	
+	public static void main(String[] args)
+	{
+		while(true)
+		{
+			neueMethode();
+		}
+	}
 }
