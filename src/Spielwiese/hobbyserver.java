9eb7c1224d8fac1eff9fb708063629d297985d17
Eth0n <maggshofmann@aol.com>
2014-01-13 20:05:18 +0100

    Spielwiese

--- src/Spielwiese/hobbyserver.java
+++ src/Spielwiese/hobbyserver.java
@@ -1,33 +1,54 @@
 package Spielwiese;
 
-import org.webbitserver.BaseWebSocketHandler;
-import org.webbitserver.WebServer;
-import org.webbitserver.WebServers;
-import org.webbitserver.WebSocketConnection;
-import org.webbitserver.handler.StaticFileHandler;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class hobbyserver extends BaseWebSocketHandler {
+
+public class hobbyserver extends Thread {
     private static int connectionCount;
-
-    public void onOpen(WebSocketConnection connection) {
-        connection.send("Hello! There are " + connectionCount + " other connections active");
-        connectionCount++;
-    }
-
-    public void onClose(WebSocketConnection connection) {
-        connectionCount--;
-    }
-
-    public void onMessage(WebSocketConnection connection, String message) {
-        connection.send(message.toUpperCase()); // echo back message in upper case
-    }
+    private static int port;
+    private static Socket server;
+    private static  BufferedReader in;
+    private static  PrintWriter out;
+    private static String txt;
 
     public static void main(String[] args) {
-        WebServer webServer = WebServers.createWebServer(8080)
-                .add("/hellowebsocket", new hobbyserver())
-                .add(new StaticFileHandler("index.html"));
-        webServer.start();
-        System.out.println("Server running at " + webServer.getUri());
-        System.out.println(connectionCount);
+    	port = 8080;
+		try {
+			server = new Socket("localhost", port);
+			System.out.print(server.getLocalPort());
+			
+			in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+			out = new PrintWriter(server.getOutputStream());
+			
+			while (true) {
+				//Jede Verbindung wird angenommen und der String an den GameHandler weitergegeben
+				try {
+					if ((txt = in.readLine()) != null) {
+						System.out.println("Server bekommt: " + txt);
+						handler.handleString(txt);
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			}//Deamon-Schleife
+		} catch (Exception e) {
+			System.out.print("Whoops! It didn't work!\n");
+			e.printStackTrace();
+		}
+	}//main
+
+	public static void close() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		}
     }
-}
