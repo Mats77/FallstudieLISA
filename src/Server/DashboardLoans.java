63554be14b62cb6fb6a0487f046a12162ee23e94
a3rosol <a3rosol@gmail.com>
2014-01-31 17:58:57 +0100

    Daumendrück

---
+++ src/Server/DashboardLoans.java
@@ -0,0 +1,44 @@
+package Server;
+
+public class DashboardLoans {
+
+	private String period;
+	private String rate;
+	private String sum;
+	private String interestsForQuarter;	
+
+	public DashboardLoans() {
+		// TODO Auto-generated constructor stub
+	}	
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getRate() {
+		return rate;
+	}
+
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+
+	public String getSum() {
+		return sum;
+	}
+
+	public void setSum(String sum) {
+		this.sum = sum;
+	}
+
+	public String getInterestsForQuarter() {
+		return interestsForQuarter;
+	}
+	public void setInterestsForQuarter(String interestsForQuarter) {
+		this.interestsForQuarter = interestsForQuarter;
+	}
+
+}
